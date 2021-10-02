889e14fca8139798ef1718705388b663a35cd791
John Eswin Nizar <jenizar@users.noreply.github.com>
2021-10-03 04:53:54 +0700

    php code
    php code

--- display.php
+++ display.php
@@ -7,15 +7,6 @@
   $data = mysqli_fetch_array($sql);
   $nilai = $data["weight"];
 
- /* if ($nilai < 1000)
-  {  
-   echo $nilai." Gr";
-  }
-  else {
-   $nilai = round($nilai/1000, 2); 
-   echo $nilai." Kg";
-  }*/
-
-  $nilai = round($nilai*0.454, 1);
+  $nilai = round($nilai*0.454, 1);   // convert from lbs to kg
   echo $nilai. " Kg";
  ?>
