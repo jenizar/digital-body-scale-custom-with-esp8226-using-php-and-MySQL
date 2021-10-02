bb35356255c6ed4572511d5e2ad3d31394b77acf
John Eswin Nizar <jenizar@users.noreply.github.com>
2021-10-03 04:52:52 +0700

    php code
    php code

--- instable.php
+++ instable.php
@@ -7,18 +7,9 @@
   $data = mysqli_fetch_array($sql);
   $i_weight = $data["weight"];
 
-  $nilai = round($i_weight*0.454, 1);
+  $nilai = round($i_weight*0.454, 1);   //convert from lbs to kg
     mysqli_query($konek, "INSERT INTO outscale (weight, unit) VALUES ('$nilai', 'Kg')");
 
-/*  if ($i_weight < 1000)
-  {  
-  //update data di database (table outscale)
-    mysqli_query($konek, "INSERT INTO outscale (weight, unit) VALUES ('$i_weight', 'Gr')");
-  }
-  else {
-    $i_weight = round($i_weight/1000, 2); 
-    mysqli_query($konek, "INSERT INTO outscale (weight, unit) VALUES ('$i_weight', 'Kg')");
-  }*/
   //update data di database (table loadcell)
   mysqli_query($konek, "UPDATE loadcell SET weight=0, unit=''");
 
